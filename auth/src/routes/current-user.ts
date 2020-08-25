4da93ebf429337a0c49c4ae84955f3e2e4ddc19c
Francis Gyimah <fgyimah883@gmail.com>
2020-08-25 14:13:33 +0000

    created the current user route handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
-router.get('/currentuser', (_req, res) => {
-  res.send('hi there');
+router.get('/currentuser', (req, res) => {
+  if (!req.session?.jwt) {
+    return res.json({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_SECRET!);
+    res.json({ currentUser: payload });
+  } catch (error) {
+    res.json({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
