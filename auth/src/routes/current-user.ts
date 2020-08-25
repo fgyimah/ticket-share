028904a87d0d7e3c42eaf0b80e9e5a2f7721f47a
Francis Gyimah <fgyimah883@gmail.com>
2020-08-25 14:40:53 +0000

    extracted current-user and require-auth into middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,11 @@
 import { Router } from 'express';
-import jwt from 'jsonwebtoken';
+
+import { currentUser } from '../middlewares/current-user';
 
 const router = Router();
 
-router.get('/currentuser', (req, res) => {
-  if (!req.session?.jwt) {
-    return res.json({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_SECRET!);
-    res.json({ currentUser: payload });
-  } catch (error) {
-    res.json({ currentUser: null });
-  }
+router.get('/currentuser', currentUser, (req, res) => {
+  res.json({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
